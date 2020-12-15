8fa3c60553bf2075b3ec1e210ce4debb3febf410
Juan Lucas Ibarra Sierra <jl.ibarra@uniandes.edu.co>
2020-12-15 18:20:26 -0500

    Manejo.py

---
+++ manejo.py
@@ -0,0 +1,20 @@
+print("Aprendiendo manejo de excepciones")
+
+
+def division(numerador, denominador):
+    try:
+        print(numerador/denominador)
+    except ZeroDivisionError:
+        print("No se puede dividir por 0. Cambiar el denominador")
+    else:
+        print("Su division fue exitosa.")
+    finally:
+        print("Operacion finalizada.")
+
+
+division(5, 0)
+
+x = -1
+
+if(x < 0):
+    raise Exception("No se reciben numeros menores a 0")
